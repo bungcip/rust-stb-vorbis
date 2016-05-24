1771c2cf46dd527aa2227ff950938e1d8553da1d
Gigih Aji Ibrahim <bungcip@gmail.com>
2016-05-24 22:30:29 +0700

    update example

--- examples/decode_filename.rs
+++ examples/decode_filename.rs
@@ -3,16 +3,11 @@
 extern crate alloc_system;
 extern crate stb_vorbis;
 
-// use stb_vorbis::*;
-
-// use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::ptr;
 use std::mem;
-use std::ffi::CString;
-
 
 // stb_vorbis_decode_filename: decode an entire file to interleaved shorts
 fn test_decode_filename(mut f: File, filename: &str)
@@ -21,9 +16,9 @@ fn test_decode_filename(mut f: File, filename: &str)
    let mut sample_rate : i32 = 0;
    let mut decoded: *mut i16 = ptr::null_mut();
    
-   let filename = CString::new(filename).unwrap();
+   let path = Path::new(filename);
    let len = unsafe { stb_vorbis::stb_vorbis_decode_filename(
-       filename.as_ptr(), 
+       &path, 
        &mut channels as *mut i32, 
        &mut sample_rate as *mut i32, 
        &mut decoded as *mut *mut i16) };
