c9dc47421dd247cc940d01f365134f18c7ac2eed
Gigih Aji Ibrahim <bungcip@gmail.com>
2016-05-29 01:50:34 +0700

    update test using ogg file from libnogg projects

--- examples/decode_filename.rs
+++ examples/decode_filename.rs
@@ -14,7 +14,7 @@ fn test_decode_filename(filename: &str) -> Vec<i16> {
     //     .read_line(&mut input_text)
     //     .expect("failed to read from stdin");
 
-    println!("stb_vorbis_decode_filename(): {}", filename);
+    println!("  stb_vorbis_decode_filename(): {}", filename);
 
     let mut channels = 0;
     let mut sample_rate = 0;
@@ -25,12 +25,12 @@ fn test_decode_filename(filename: &str) -> Vec<i16> {
         &mut sample_rate,
         &mut decoded);
 
-    println!("    decode success, len: {} bytes, buffer length: {} elements", len, decoded.len());
+    println!("    SUCCESS, len: {} bytes, buffer length: {} elements", len, decoded.len());
     return decoded;
 }
 
 fn test_decode_memory(filename: &str) -> Vec<i16> {
-    println!("stb_vorbis_decode_memory(): {}", filename);
+    println!("  stb_vorbis_decode_memory(): {}", filename);
 
     //  load ogg file to memory
     let mut f = File::open(filename).unwrap();
@@ -47,7 +47,7 @@ fn test_decode_memory(filename: &str) -> Vec<i16> {
         &mut decoded
     );
 
-    println!("    decode success, len: {} bytes, buffer length: {} elements",
+    println!("    SUCCESS, len: {} bytes, buffer length: {} elements",
         len,
         decoded.len());
         
@@ -63,12 +63,14 @@ fn main() {
     let output = Path::new(&output);
     let mut output = File::create(output).unwrap();
 
+    println!("{}", filename);
+
     let data1 = test_decode_filename(filename);
     let data2 = test_decode_memory(filename); 
 
     // must be same
     if data1 != data2 {
-        println!("Error: decode_filename() != decode_memory");
+        println!("  Error: decode_filename() != decode_memory");
         std::process::exit(1);
     }
 
--- tests/compare.py
+++ tests/compare.py
@@ -4,6 +4,7 @@ import hashlib
 import subprocess
 import sys
 import os
+import time
 
 # compare output file between original stb_vorbis and rust port
 #
@@ -21,12 +22,35 @@ def md5(fname):
 
 oggs = [
     'single-code-2bits', 'single-code-nonsparse', 'single-code-ordered', 'single-code-sparse',
-    'stereo_short', 'mono'
+    '6ch-all-page-types', '6ch-long-first-packet', '6ch-moving-sine', '6ch-moving-sine-floor0',
+    'bitrate-123', 'bitrate-456-0', 'bitrate-456-789',
+    'empty-page',
+    'noise-6ch', 'noise-stereo',
+    'sample-rate-max',
+    'sketch008-floor0', 
+    'square', 'square-interleaved', 'square-stereo', 
+    'thingy-floor0',
+    #'stereo_short', 'mono', ~~ cannot be distributed, this is a just song from my playlist...
+    
+    # rust-stb-vorbis
+    # 'thingy', 'sketch008', 'sketch039', ~~ very slow
+    
+    # also crash in original C stb-vorbis
+    # no need to fix this....
+        
+    #'6-mode-bits', '6-mode-bits-multipage',
+    #'large-pages',
+    #'long-short',
+    #'zero-length',
+    #'partial-granule-position',
+    #'square-with-junk', 
+    #'bad-continued-packet-flag', 
 ]
 
 # run stb_vorbis
 #print("run stb_vorbic C...")
 #for o in oggs:
+#    print("  run {}".format(o))
 #    subprocess.call(["vorvis-sample.exe", "1", "ogg/{}.ogg".format(o), "c_output/[decode_filename]_{}.out".format(o)])
 #    subprocess.call(["vorvis-sample.exe", "5", "ogg/{}.ogg".format(o), "c_output/[decode_frame_pushdata]_{}.out".format(o)])
 
@@ -60,10 +84,12 @@ for bin in binaries:
 
         try:
             input = "ogg/{}.ogg".format(i)
+            start_time = time.time()
             return_value = subprocess.call([executable, input, rust_name])
             if return_value != 0:
                 print("stoped due to error...")
                 sys.exit()
+            end_time = time.time()
         except:
             print("error happened")
             sys.exit()
@@ -81,5 +107,5 @@ for bin in binaries:
             print("  [WRONG] rust output have wrong hash! filename: {}, size: {}".format(filename, rust_size))
             sys.exit()
 
-        print("  {} [OK]".format(filename))
+        print("  [OK] in {:.3f} seconds".format(end_time - start_time))
 
